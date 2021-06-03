3c29c1f87d2a3bbd1c10220bbcb6605efe680517
Reyansh Mishra <r8.reyansh@gmail.com>
2021-06-03 16:50:35 +0530

    added print statement in division

--- src/division.java
+++ src/division.java
@@ -2,6 +2,6 @@ public class division {
     public static void main(String[] args) {
         int a = 20;
         int b = 10;
-        System.out.println(a/b);
+        System.out.println("Division of two numbers is: " +(a/b));
     }
 }
